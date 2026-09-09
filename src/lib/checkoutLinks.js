d33d779e3843bbc6eb578f83d61d9bf6ec5032dd
gcportilho76 <gcportilho@gmail.com>
2026-09-09 09:38:23 -0300

    Update checkoutLinks.js

--- src/lib/checkoutLinks.js
+++ src/lib/checkoutLinks.js
@@ -3,7 +3,7 @@
 // telas de CTA dentro dos módulos, etc.). Troque pelos links reais
 // de checkout da Kiwify (ou Hotmart) de cada produto.
 export const CHECKOUT_LINKS = {
-  vol2_access: 'https://pay.kiwify.com.br/SEU_LINK_VOLUME_2',
-  vol3_access: 'https://pay.kiwify.com.br/SEU_LINK_VOLUME_3',
-  vol4_access: 'https://pay.kiwify.com.br/SEU_LINK_VOLUME_4',
+  vol2_access: 'https://pay.kiwify.com.br/EhJKGjr',
+  vol3_access: 'https://pay.kiwify.com.br/EhJKGjr',
+  vol4_access: 'https://pay.kiwify.com.br/EhJKGjr',
 };
